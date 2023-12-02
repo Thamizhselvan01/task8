e521d6e11622c4084babacc5db6b1a018328e438
Thamilzhselvan5 <tamilselvank2823.com>
2023-12-02 16:58:41 +0530

    mmm

--- index.js
+++ index.js
@@ -10,6 +10,17 @@ app.use(cors());
 const PORT = 5000;
 
 app.use("/api", HallRouter);
+app.get("/", async (req, res) => {
+  try {
+    res.status(200).json({
+      message: "hall booking  API is Working Fine....",
+    });
+    console.log("HAll booking API is Working Fine....");
+  } catch (error) {
+    res.status(500).json({ message: "server error" });
+    console.log("error in server>>>>>>>");
+  }
+});
 
 app.listen(PORT, () => {
   console.log("app is listening in the port>>>", PORT);
